2183df63ad36d61a0383d71ae24818442bff4a4c
nishayadav30 <50619295+nishayadav30@users.noreply.github.com>
2022-11-14 09:41:57 +0530

    Assignment 3 (First Commit)

---
+++ crudoperations/src/main/java/com/example/crudoperations/entity/studentRequest.java
@@ -0,0 +1,29 @@
+package com.example.crudoperations.entity;
+
+public class studentRequest {
+	
+	public int id;
+	public String name;
+	public int marks;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	
+	
+
+}
